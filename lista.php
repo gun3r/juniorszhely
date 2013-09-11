f015f495bb99dcef7bb484a7bc7350ed9455cb95
vadasz.gergo@gmail.com <vadasz.gergo@gmail.com@4fec9e71-55bf-351e-69b5-70d343217ec3>
2013-09-11 19:36:43 +0000

    lista.php külső csinosítgatása, semmi lényeges.

--- lista.php
+++ lista.php
@@ -1,37 +1,24 @@
-<?php
-include 'connection.php';
-
-$sql = "SELECT * FROM data";
-
-
-$res = mysqli_query($con, $sql);
-
-echo "<table border='1'>
+<html>
+<body bgcolor="#D8D8D8">
+<table border='8'>
 <tr>
 <th>A szám</th>
 <th>Régi eszköz</th>
 <th>Új eszköz</th>
 <th>Név</th>
 <th>Dátum</th>
-</tr>";
-while($sor = mysqli_fetch_array($res)) {
-
-if($sor['closed']==0){
- echo "<tr>";
- echo "<td>" . $sor['a_szam'] . "</td>";
- echo "<td>" . $sor['serial1'] . "</td>";
- echo "<td>" . $sor['serial2'] . "</td>";
- echo "<td>" . $sor['sp_code'] . "</td>";
- echo "<td>" . $sor['date'] . "</td>";
- echo "<td>
- <form action=\"mod.php\" method=\"post\">
- <input type=\"hidden\" name=\"id\" value=" . $sor['id'] . ">
- <input type=\"submit\" value=\"OK\">
- </form> </td>";
- echo "</tr>"; 
-}
-}
-echo "</table>";
-  
-mysqli_close($con);
-?>
+</tr><tr><td>A0698671</td><td>E31NAAA01008</td><td>J331008868</td><td>sp4108</td><td>2013-09-10</td><td>
+ <form action="mod.php" method="post">
+ <input type="hidden" name="id" value=1>
+ <input type="submit" value="OK">
+ </form> </td></tr><tr><td>TESZT</td><td>TESZT</td><td>TESZT</td><td>TESZT</td><td>2013-09-10</td><td>
+ <form action="mod.php" method="post">
+ <input type="hidden" name="id" value=2>
+ <input type="submit" value="OK">
+ </form> </td></tr><tr><td>A1112222</td><td>16402Y000923</td><td>ZTEEF01BAH09123</td><td>sp4109</td><td>2013-09-10</td><td>
+ <form action="mod.php" method="post">
+ <input type="hidden" name="id" value=3>
+ <input type="submit" value="OK">
+ </form> </td></tr></table>
+</body>
+</html>
