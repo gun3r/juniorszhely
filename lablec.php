827176ddc6c372b98e5de847f325952df871cc41
vadasz.gergo@gmail.com <vadasz.gergo@gmail.com@4fec9e71-55bf-351e-69b5-70d343217ec3>
2013-10-03 11:15:02 +0000

    a

--- lablec.php
+++ lablec.php
@@ -1,15 +1,7 @@
 <?php
 include 'kuki.php';
 
-
-//név kiírás
-//echo "<form method=\"post\" action=\"eszkozcsere.php\"><input type=\"submit\" value=\"Eszközcsere\" name=\"submit\"/></form>";
-//echo "<form method=\"post\" action=\"lista.php\"><input type=\"submit\" value=\"Lista\" name=\"submit\"/></form>";
-
 echo "<h4>Készítették: Molnár Gábor, Vadász Gergő </br> ";
 echo "	  <a href='mailto:molnar.gabor2@telekom.hu;vadasz.gergo@telekom.hu?Subject=Eszközcsere'> E-mail a fejlesztőknek</a> </h4>  ";
-//echo "";
-
-
 
 ?>
